c6bca7f87cd40fbc70a0814e6fb3a1a5a0dbf6cf
Ahmed Mongy <amabdelmongy@gmail.com>
2022-09-22 15:38:22 +0200

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.62.0"
+    }
+  }
+  backend "s3" {
+    bucket = "amabdelmongy-terraform-state"
+    key    = "terraform-aws-docker-deploy/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      CreatedBy = "terraform"
+    }
+  }
+}
+
+output "alb_dns" {
+  value = aws_lb.instance.dns_name
+}
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.instance.name
+}
